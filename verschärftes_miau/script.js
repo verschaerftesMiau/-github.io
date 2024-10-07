fcb289742b86735c64dbd3ae4a47d6e2b18f3f1a
verschaerftesMiau <tornadaerg@gmail.com>
2024-10-07 16:47:53 +0200

    Add files via upload

---
+++ verschärftes_miau/script.js
@@ -0,0 +1,8 @@
+/* Get the button, and when the user clicks on it, execute myFunction
+var x
+document.getElementById("myBtn").onclick = function() {myFunction()};
+
+ myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content 
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
